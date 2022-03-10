f558cfeff9783c8f7ea38205fc11339f365b3e32
shilinaijiali <87008920+shilinaijiali@users.noreply.github.com>
2022-03-10 16:05:13 +0800

    Add files via upload

---
+++ stu/views.py
@@ -0,0 +1,63 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import StudentForm
+from django.shortcuts import render
+from .models import Student
+from django.views import View
+
+
+# Create your views here.
+
+def index(request):
+    students = Student.get_all()
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            # cleaned_data = form.cleaned_data
+            # student = Student()
+            # student.name = cleaned_data['name']
+            # student.sex = cleaned_data['sex']
+            # student.email = cleaned_data['email']
+            # student.profession = cleaned_data['profession']
+            # student.qq = cleaned_data['qq']
+            # student.phone = cleaned_data['phone']
+            # student.save()
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = StudentForm()
+    context = {
+        'students': students,
+        'form': form,
+    }
+    return render(request, 'index.html', context=context)
+
+
+class IndexView(View):
+    template_name = 'index.html'
+
+    def get_context(self):
+        students = Student.get_all()
+        context = {
+            'student': students,
+        }
+        return context
+
+    def get(self, request):
+        context = self.get_context()
+        form = StudentForm()
+        context.update({
+            'form': form
+        })
+        return render(request, self.template_name, context=context)
+
+    def post(self, request):
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
+        context = self.get_context()
+        context.update({
+            'form': form
+        })
+        return render(request, self.template_name, context=context)
